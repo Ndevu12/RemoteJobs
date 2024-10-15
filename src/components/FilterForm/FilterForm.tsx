f0f922abaeb23d3dcb16c2861e1f248510253b78
Jean Paul Elisa <niyokwizerwajeanpaulelisa@gmail.com>
2024-10-15 13:49:14 +0200

    fix<header> improve header responsiveness

--- src/components/FilterForm/FilterForm.tsx
+++ src/components/FilterForm/FilterForm.tsx
@@ -25,17 +25,17 @@ const FilterForm = (props: Props) => {
   };
 
   return (
-    <form onSubmit={onFilterSubmit} className="w-[20%] flex flex-col md:flex-row items-center mr-4 gap-4 p-4 rounded-md">
-      <div className="flex items-center w-full md:w-2/3 bg-gray-100 p-2 rounded-md">
+    <form onSubmit={onFilterSubmit} className="w-full md:w-1/2 lg:w-1/3 flex flex-col md:flex-row items-center gap-4 p-4 rounded-md">
+      <div className="flex items-center w-full bg-gray-100 p-2 rounded-md">
         <Search className="text-gray-500 mr-2" />
         <input
           type="text"
           name="title"
-          className="flex-grow bg-transparent border-none outline-none p-2"
+          className="flex-grow bg-transparent border-none outline-none p-2 placeholder:text-base md:placeholder:text-lg lg:placeholder:text-xl"
           placeholder="Filter by job title"
         />
       </div>
-      <div className="flex items-center w-full md:w-1/3">
+      <div className="flex items-center w-full">
         <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-md flex items-center justify-center">
           <Search className="mr-2" />
           Search
